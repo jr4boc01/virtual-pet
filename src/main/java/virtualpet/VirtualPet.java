3738be7b6182cca0a486b5437ca4c7dcae271e21
Joe Rudy <joe.rudy@sbcglobal.net>
2018-09-16 09:21:35 -0400

    Subsequent code commit

--- src/main/java/virtualpet/VirtualPet.java
+++ src/main/java/virtualpet/VirtualPet.java
@@ -1,5 +1,58 @@
 package virtualpet;
 
 public class VirtualPet {
+	String petAction; // User provided action - feed, hydrate, pet
+	int hungerLevel; // Level of hunger
+	int hydrationLevel; // Level of hydration
+	int happinessLevel; // Level of happiness
+
+// Pet constructor 
+//	
+	public VirtualPet(int initialHungerLevel, int initialHydrationLevel, int initialHappinessLevel) {
+		hungerLevel = initialHungerLevel;
+		hydrationLevel = initialHydrationLevel;
+		happinessLevel = initialHappinessLevel;
+	}
+
+// Pet methods
+//	
+	public void feedPet() {
+		hungerLevel -= 2;
+
+	}
+
+	public void hydratePet() {
+		hydrationLevel += 2;
+
+	}
+
+	public void petPet() {
+		happinessLevel += 2;
+
+	}
+
+	public void displayStatus() {
+		System.out.println();
+		System.out.println("Charlie the Chia Pet has the following status: ");
+		System.out.println(" Hunger level:    " + hungerLevel);
+		System.out.println(" Hydration level: " + hydrationLevel);
+		System.out.println(" Happiness level: " + happinessLevel);
+
+		System.out.println(" ");
+		System.out.println("What would you like to do for Charlie? ");
+		System.out.println("1. Feed ");
+		System.out.println("2. Give water ");
+		System.out.println("3. Pet ");
+		System.out.println("4. Exit game");
+		System.out.println("> ");
+
+	}
+
+	public void tick() {
+		hungerLevel += 1;
+		hydrationLevel -= 1;
+		happinessLevel -= 1;
+
+	}
 
 }
--- src/main/java/virtualpet/VirtualPetApp.java
+++ src/main/java/virtualpet/VirtualPetApp.java
@@ -1,5 +1,65 @@
 package virtualpet;
 
+import java.util.Scanner;
+
 public class VirtualPetApp {
+
+	public static void main(String[] args) {
+		VirtualPet myPet = new VirtualPet(0, 10, 10);
+
+		// Display initial pet status
+		myPet.displayStatus();
+
+		Scanner input = new Scanner(System.in);
+		boolean gameOn = true;
+
+		do {
+			int userAction = input.nextInt();
+			input.nextLine();
+			if (userAction > 4) {
+				// Test for valid user input
+				System.out.println("Invalid entry, please try again...");
+				System.out.println();
+				myPet.displayStatus();
+			} else if (userAction == 4) {
+				// See if user wants to exit game
+				gameOn = false;
+				System.out.println("Thanks for playing!!!");
+			} else if (userAction == 1) {
+				// User wants to feed the Chia
+				myPet.feedPet();
+				myPet.tick();
+				System.out.println("Charlie says \"Yum! Thanks for the chow!\"");
+				myPet.displayStatus();
+			} else if (userAction == 2) {
+				// User wants to give Chia water
+				myPet.hydratePet();
+				myPet.tick();
+				System.out.println("Charlie says \"Burp! Thanks for the drink!\"");
+				myPet.displayStatus();
+			} else if (userAction == 3) {
+				// User wants to pet Chia
+				myPet.petPet();
+				myPet.tick();
+				System.out.println("Charlie says \"Ahhhh! I'm happy now!\"");
+				myPet.displayStatus();
+			} else {
+				// exception code
+				System.out.println("Program error msg 1");
+				gameOn = false;
+			}
+			if (myPet.hydrationLevel < 3) {
+				// Test if Charlie has died out of neglect - dehydration
+				System.out.println("Game Over - Charlie the Chia has died of dehydration!");
+				gameOn = false;
+			} else if (myPet.hungerLevel > 8) {
+				// Test if Charlie has died out of neglect - starvation
+				System.out.println("Game Over - Charlie the Chia has died of starvation!");
+				gameOn = false;
+			}
+		} while (gameOn = true);
+
+		input.close();
+	}
 
 }
